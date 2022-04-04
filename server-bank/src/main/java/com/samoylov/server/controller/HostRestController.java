45080e1cd573f4d0435121fa67a8f7f9cd6058dc
Igor Samoylov <samoylov11gor@gmail.com>
2022-04-05 01:12:36 +0300

    create HostRestController

---
+++ server-bank/src/main/java/com/samoylov/server/controller/HostRestController.java
@@ -0,0 +1,32 @@
+package com.samoylov.server.controller;
+import com.samoylov.dto.CardDTO;
+import com.samoylov.dto.CustomerDTO;
+import com.samoylov.server.service.AccountService;
+import com.samoylov.server.service.CustomerServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class HostRestController {
+    private CustomerServiceImpl customerService;
+    private AccountService accountService;
+
+    @GetMapping("customers")
+    List<CustomerDTO> getCustomerInfo(){
+        List<CustomerDTO> customerDTOS = customerService.getAllCustomers();
+        if (customerDTOS.size() == 0)
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT, "No Customers");
+        else return customerDTOS;
+    }
+
+    @PostMapping("customer/account/balance")
+    Long getCustomerBalance(@RequestBody CardDTO cardDTO){
+        return accountService.getBalanceByCard(cardDTO.getCard_number(), cardDTO.getPin())
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.INTERNAL_SERVER_ERROR, "Something went Wrong"));
+    }
+}
