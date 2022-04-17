c11e784f09fa55391d730a7f9c54d6ffd107a019
Igor Samoylov <samoylov11gor@gmail.com>
2022-04-17 21:03:48 +0300

    modify Host controller for spring security features

--- server-bank/src/main/java/com/samoylov/server/controller/HostRestController.java
+++ server-bank/src/main/java/com/samoylov/server/controller/HostRestController.java
@@ -2,15 +2,15 @@ package com.samoylov.server.controller;
 
 import com.samoylov.dto.CardDTO;
 import com.samoylov.dto.CustomerDTO;
+import com.samoylov.server.security.JwtTokenProvider;
 import com.samoylov.server.service.AccountService;
 import com.samoylov.server.service.CardService;
 import com.samoylov.server.service.CustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -19,21 +19,27 @@ import java.util.List;
 public class HostRestController {
     private CustomerService customerService;
     private AccountService accountService;
+    private JwtTokenProvider jwtTokenProvider;
     private CardService cardService;
 
-    @GetMapping("customers")
+    @GetMapping("/customers")
     List<CustomerDTO> getCustomerInfo() {
         return customerService.getAllCustomers();
     }
 
-    @PostMapping("customer/account/balance")
-    BigDecimal getCustomerBalance(@RequestBody CardDTO cardDTO) {
-        return accountService.getBalanceByCard(cardDTO.getCard_number(), cardDTO.getPin());
-
+    @GetMapping("/customer")
+    CustomerDTO getCustomer(HttpServletRequest request) {
+        String token = request.getHeader("authorization").substring(7);
+        String cardNumber = jwtTokenProvider.getUsername(token);
+        CustomerDTO customerDTO = customerService.getCustomerByCard(cardNumber);
+        return customerDTO;
     }
 
-    @PostMapping("customer/info")
-    CustomerDTO getCustomerByCard(@RequestBody CardDTO cardDTO) {
-        return customerService.getCustomerByCard(cardDTO.getCard_number(), cardDTO.getPin());
+    @GetMapping("/customer/account/balance")
+    BigDecimal getCustomerBalance(HttpServletRequest request) {
+        String token = request.getHeader("authorization").substring(7);
+        String cardNumber = jwtTokenProvider.getUsername(token);
+        CardDTO cardDTO = cardService.getCardByNumber(cardNumber);
+        return accountService.getBalanceByCard(cardDTO.getCardNumber());
     }
 }
