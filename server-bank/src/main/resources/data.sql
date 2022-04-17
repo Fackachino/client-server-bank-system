709ea8dd32595b2655cedf015d0537a7fa85e5bc
Igor Samoylov <samoylov11gor@gmail.com>
2022-04-17 23:22:23 +0300

    add new customer to data

--- server-bank/src/main/resources/data.sql
+++ server-bank/src/main/resources/data.sql
@@ -29,15 +29,18 @@ CREATE TABLE Card(
 INSERT INTO Customer(first_name,second_name,patronymic) VALUES
 ('Mark','Ivanuk','Lvovich'),
 ('Igor', 'Samoylov', 'Alekseevich'),
-('Anton', 'Kiselev', 'Yurievich');
+('Anton', 'Kiselev', 'Yurievich'),
+('Motuzova', 'Diana', 'Vladimirovna');
 
 
 INSERT INTO Account(id_customer, account_number, balance) VALUES
 ('1', '100001', '3000.00'),
 ('2', '100002', '123000.00'),
-('3', '100003', '1230.00');
+('3', '100003', '1230.00'),
+('4', '100005', '66.00');
 
 INSERT INTO Card(id_account, card_number, pin) VALUES
 ('1', '5222232132139321', '$2a$12$oHDx7c88c6OX/TOds.CsNeBZy33v.jAB710y6b5m4Os0jPyhly2za'),
 ('2', '5223232108348731', '$2a$12$U8QUOq72EeDFiKNY8mQjMuond8FDL5bawDgMhYSiJFsmomqxx4m8y'),
-('3', '5223095139345432', '$2a$12$hTbFq.f6cJFk2S7Uhxl3QuFR0D3xtilwHaNRlV1Bib6DSZvOKDEai');
+('3', '5223095139345432', '$2a$12$hTbFq.f6cJFk2S7Uhxl3QuFR0D3xtilwHaNRlV1Bib6DSZvOKDEai'),
+('4', '1488666777666123', '$2a$12$U8QUOq72EeDFiKNY8mQjMuond8FDL5bawDgMhYSiJFsmomqxx4m8y');
