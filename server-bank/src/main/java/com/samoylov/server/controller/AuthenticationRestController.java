fd69dfac0040888fface325e2d4529dfa0d184a7
Igor Samoylov <samoylov11gor@gmail.com>
2022-04-18 12:13:54 +0300

    modify

--- server-bank/src/main/java/com/samoylov/server/controller/AuthenticationRestController.java
+++ server-bank/src/main/java/com/samoylov/server/controller/AuthenticationRestController.java
@@ -1,12 +1,11 @@
 package com.samoylov.server.controller;
 
-import com.samoylov.dto.CardDTO;
 import com.samoylov.dto.ClientAuthRequest;
+import com.samoylov.dto.ServerAuthResponse;
 import com.samoylov.server.exception.WrongPinException;
 import com.samoylov.server.security.JwtTokenProvider;
 import com.samoylov.server.service.CardService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
@@ -14,9 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/auth")
@@ -27,17 +23,12 @@ public class AuthenticationRestController {
     private CardService cardService;
 
     @PostMapping("/login")
-    public ResponseEntity login(@RequestBody ClientAuthRequest clientAuthRequest) {
+    public ServerAuthResponse login(@RequestBody ClientAuthRequest clientAuthRequest) {
         try {
             String cardNumber = cardService.getCardByNumber(clientAuthRequest.getCardNumber()).getCardNumber();
             authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(cardNumber, clientAuthRequest.getPin()));
             String token = jwtTokenProvider.createToken(cardNumber);
-
-            Map<String, String> response = new HashMap<>();
-            response.put("cardNumber", cardNumber);
-            response.put("token", token);
-
-            return ResponseEntity.ok(response);
+            return new ServerAuthResponse(cardNumber, token);
         } catch (AuthenticationException e) {
             throw new WrongPinException("Invalid password");
         }
