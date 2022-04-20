76412b8914f510f9da5356e6ba1aae6d473955ff
Igor Samoylov <samoylov11gor@gmail.com>
2022-04-20 13:07:07 +0300

    add deposit and withdraw money features

--- server-bank/src/main/java/com/samoylov/server/controller/HostRestController.java
+++ server-bank/src/main/java/com/samoylov/server/controller/HostRestController.java
@@ -1,6 +1,7 @@
 package com.samoylov.server.controller;
 
 import com.samoylov.dto.CardDTO;
+import com.samoylov.dto.ClinetMoneyDTO;
 import com.samoylov.dto.CustomerDTO;
 import com.samoylov.server.security.JwtTokenProvider;
 import com.samoylov.server.service.AccountService;
@@ -11,7 +12,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -29,17 +29,34 @@ public class HostRestController {
 
     @GetMapping("/customer")
     CustomerDTO getCustomer(HttpServletRequest request){
-        String token = request.getHeader("authorization").substring(7);
-        String cardNumber = jwtTokenProvider.getUsername(token);
+        String cardNumber = getCardNumber(request);
         CustomerDTO customerDTO = customerService.getCustomerByCard(cardNumber);
         return customerDTO;
     }
 
     @GetMapping("/customer/account/balance")
     BigDecimal getCustomerBalance(HttpServletRequest request){
-        String token = request.getHeader("authorization").substring(7);
-        String cardNumber = jwtTokenProvider.getUsername(token);
+        String cardNumber = getCardNumber(request);
         CardDTO cardDTO = cardService.getCardByNumber(cardNumber);
         return accountService.getBalanceByCard(cardDTO.getCardNumber());
+    }
+
+    @PostMapping("/customer/account/balance/deposit")
+    BigDecimal depositMoney(@RequestBody ClinetMoneyDTO moneyRequest, HttpServletRequest request){
+        String cardNumber = getCardNumber(request);
+        accountService.depositMoney(cardNumber, moneyRequest.getMoney());
+        return accountService.getBalanceByCard(cardNumber);
+    }
+
+    @PostMapping("/customer/account/balance/withdraw")
+    BigDecimal withdrawMoney(@RequestBody ClinetMoneyDTO moneyRequest, HttpServletRequest request){
+        String cardNumber = getCardNumber(request);
+        accountService.withDrawMoney(cardNumber, moneyRequest.getMoney());
+        return accountService.getBalanceByCard(cardNumber);
+    }
+
+    private String getCardNumber(HttpServletRequest request){
+        String token = request.getHeader("authorization").substring(7);
+        return jwtTokenProvider.getUsername(token);
     }
 }
--- server-bank/src/main/java/com/samoylov/server/service/AccountService.java
+++ server-bank/src/main/java/com/samoylov/server/service/AccountService.java
@@ -2,13 +2,16 @@ package com.samoylov.server.service;
 
 import com.samoylov.dto.AccountDTO;
 import com.samoylov.dto.CardDTO;
+import com.samoylov.server.entity.Account;
 import com.samoylov.server.exception.AccountNotFoundException;
+import com.samoylov.server.exception.BalanceException;
 import com.samoylov.server.repository.AccountRepository;
 import com.samoylov.server.service.utility.AccountEntityConverter;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Service
 @AllArgsConstructor
@@ -22,8 +25,32 @@ public class AccountService {
 
     public AccountDTO getAccountByCard(String cardNumber) {
         CardDTO cardDTO = cardService.getCardByNumber(cardNumber);
+        return AccountEntityConverter.convertToDto(getAccount(cardNumber));
+    }
+
+    public BigDecimal depositMoney(String cardNumber, BigDecimal money) {
+        Account account = getAccount(cardNumber);
+        account.setBalance(account.getBalance().add(money));
+        accountRepository.save(account);
+        return account.getBalance();
+    }
+
+    public BigDecimal withDrawMoney(String cardNumber, BigDecimal money) {
+        Account account = getAccount(cardNumber);
+
+        BigDecimal balance = account.getBalance();
+        if (balance.compareTo(money) < 0) {
+            throw new BalanceException("Not enough money to withdraw");
+        }
+
+        account.setBalance(account.getBalance().subtract(money));
+        accountRepository.save(account);
+        return account.getBalance();
+    }
+
+    private Account getAccount(String cardNumber) {
+        CardDTO cardDTO = cardService.getCardByNumber(cardNumber);
         return accountRepository.getAccountByCard(cardDTO.getCardNumber())
-                .map(AccountEntityConverter::convertToDto)
                 .orElseThrow(() -> new AccountNotFoundException(
                         "Account not found with card: " + cardDTO.getCardNumber()));
     }
